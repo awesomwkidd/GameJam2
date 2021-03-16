1c4dc0b16d41b2c1695e95f98a94bc6fd3f77028
awesomwkidd <79286272+awesomwkidd@users.noreply.github.com>
2021-03-16 13:04:57 -0400

    Update SprayCan.lua

--- Tools/SprayCan.lua
+++ Tools/SprayCan.lua
@@ -4,10 +4,29 @@
 The script handeler for the spray can tool!
 ]]
 
+
 --//Variables
 
---[[
-add raycasting stuff here, to lazy to do it rn 
-]]
+--// Script doesn't start till the local player is found
+repeat wait() until game.Players.LocalPlayer
 
---//Main
+local player = game.Players.LocalPlayer
+local chr = player.Character
+local mouse = player:GetMouse()
+local tool = script.Parent
+
+--//Keybinds 
+mouse.KeyDown:Connect(function(key)
+    if key == "e" then
+      
+   --// Firing the spray event without name, making it easier on me lol
+        for __, val in pairs(script.Parent:WaitForChild("SprayEvents"):GetChildren()) do
+           if val.ClassName == "RemoteEvent" then
+              val:FireServer()
+           end
+        end
+      
+      
+    end
+end
+
