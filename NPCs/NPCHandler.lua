351a5374ba2c15e8c191d4e1dd26bbfda7613418
awesomwkidd <79286272+awesomwkidd@users.noreply.github.com>
2021-03-16 08:45:09 -0400

    Update NPCHandler.lua

--- NPCs/NPCHandler.lua
+++ NPCs/NPCHandler.lua
@@ -1,1 +1,16 @@
-print("hi")
+--[[
+
+-=- Created by awesomwkidd -=-
+
+The NPC handler!
+
+]]
+--//Variables
+local workspace = game:GetService("Workspace")
+local NPCLocation = workspace:FindFirstChild("NPCs")
+
+--// List of NPCs
+local NPCs = {
+  "John" = NPCLocation:WaitForChild("John"),
+  
+}
