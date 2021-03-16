fd7f6c0743b6bf0be76fdef557c2a5f6c5cae49d
awesomwkidd <79286272+awesomwkidd@users.noreply.github.com>
2021-03-16 12:58:59 -0400

    Create GameHandler.lua

---
+++ Main/GameHandler.lua
@@ -0,0 +1,16 @@
+game:GetService("Players").PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(chr)
+        
+        --// Where values are held for in-game functions
+        local dataValues = Instance.new("Folder")
+        dataValues.Name = "Values"
+        dataValues.Parent = player
+        
+        --// To check if player is wanted by school officials or not
+        local wantedValue = Instance.new("BoolValue")
+        wantedValue.Name = "Wanted"
+        wantedValue.Value = false
+        wantedValue.Parent = dataValues
+ 
+    end)
+end)
