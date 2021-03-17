fd09d3ac63390ecdc68e83c8c15e27d48b064735
awesomwkidd <79286272+awesomwkidd@users.noreply.github.com>
2021-03-16 20:46:11 -0400

    Update NPCHandler.lua

--- NPCs/NPCHandler.lua
+++ NPCs/NPCHandler.lua
@@ -8,38 +8,53 @@ I've literally no clue if this works or not, i'm working on this in school, I'm 
 
 ]]
 --//Variables
-local workspace = game:GetService("Workspace")
 local NPCLocation = workspace:FindFirstChild("NPCs")
 local Boxes = workspace:FindFirstChild("Boxes")
+local pathfindingService = game:GetService("PathfindingService")
 
 --//Value for testing
 local moveTo = true
 
 --// List of NPCs
-local NPCs = {
-  John = NPCLocation:WaitForChild("John"),
-  Principal = NPCLocation:WaitForChild("Principal")
-}
+local John = NPCLocation:WaitForChild("John")
+local Principal = NPCLocation:WaitForChild("Principal")
 
 --// List of parts that NPCs move to.
 local Boxes = {
-  --// Prinicpal boxes
-  PrincipalBox1 = Boxes.Principal:FindFirstChild("PrincipalBox1"),
-  
-  --// John boxes
-  JohnBox1 = Boxes.John:FindFirstChild("JohnBox1"),
+	--// Prinicpal boxes
+	PrincipalBox1 = Boxes.Principal:FindFirstChild("PrincipalBox1"),
+
+	--// John boxes
+	JohnBox1 = Boxes.John:FindFirstChild("JohnBox1"),
 }
 
-local function NPCMoveTo(npcHum, objectToMove)
-   npcHum:MoveTo(objectToMove)
-end
+local function NPCMove(NPC, NPChum, destination)
+	local humanoid = NPChum
+	local body = NPC:FindFirstChild("HumanoidRootPart") or script.Parent:FindFirstChild("Torso")
+	local destination = destination
 
+	local path = pathfindingService:CreatePath()
+	path:ComputeAsync(body.Position, destination)
+	
+	local waypoints = path:GetWaypoints()
+
+	for __, waypoint in pairs(waypoints) do
+		humanoid:MoveTo(waypoint.Position)
+
+		if waypoint.Action == Enum.PathWaypointAction.Jump then
+			humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+		end
+
+		humanoid.MoveToFinished:Wait()
+	end
+	
+end
 
 --//Main
 if moveTo == true then
-   NPCMoveTo(NPCs.John, Boxes.JohnBox1)
+	NPCMove(John, John:WaitForChild("Humanoid"), Boxes.JohnBox1.Position)
 else
-   return moveTo
+	return moveTo
 end
 
 
